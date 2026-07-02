0a970dbd2d10c37233f33d8f4f6bdb4b19a22832
Raziq <muhdraziqcoutinho@gmail.com>
2026-07-02 20:33:43 +0800

    update items

--- app.js
+++ app.js
@@ -11,24 +11,33 @@ app.get("/items", (req, res) => {
   res.json(groceryList);
 });
 
-app.post('/items', (req, res) => {
+app.post("/items", (req, res) => {
   let newItem = {
     id: Date.now(),
     name: req.body.name,
-    completed: false
+    completed: false,
   };
   groceryList.push(newItem);
-  res.status(201).json(newItem); 
+  res.status(201).json(newItem);
 });
 
-app.delete('/items/:id', (req, res) => {
+app.put("/items/:id", (req, res) => {
   let id = parseInt(req.params.id);
-  groceryList = groceryList.filter(i => i.id !== id);
-  res.json({ message: 'Deleted successfully'});
+  let index = groceryList.findIndex((i) => i.id === id);
+
+  if (index !== -1) {
+    groceryList[index] = { ...groceryList[index], ...req.body };
+    res.json(groceryList[index]);
+  } else {
+    res.status(404).json({ error: "Item not found" });
+  }
 });
 
-
-
+app.delete("/items/:id", (req, res) => {
+  let id = parseInt(req.params.id);
+  groceryList = groceryList.filter((i) => i.id !== id);
+  res.json({ message: "Deleted successfully" });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
