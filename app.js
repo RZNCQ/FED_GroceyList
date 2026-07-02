501c40358c1ceefcebfaae1c636bf92e1e0b3233
Raziq <muhdraziqcoutinho@gmail.com>
2026-07-02 20:21:21 +0800

    add api get

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-const express = require('express');
-
+const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.json()); // parse JSON body
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.send('Hello from Node.js API!');
+let groceryList = [];
+
+app.get("/items", (req, res) => {
+  res.json(groceryList);
+});
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
